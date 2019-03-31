1ba82c888c27901b8b8fee6559ffce4b78957d75
Dariusz Tomaszewski <dtom@stibosystems.com>
2019-03-31 13:52:10 +0200

    getdata for payments-api service

--- libs/payments/data-access/src/lib/payments-api.service.ts
+++ libs/payments/data-access/src/lib/payments-api.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentsApiService {
   constructor() {}
+
+  getData(): Observable<{}> {
+    return of({});
+  }
 }
